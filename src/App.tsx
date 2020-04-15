a05f7da263d162a7f89176f425ff93ea20e8fb11
NEON <danielemmanuel247@gmail.com>
2020-04-15 10:21:47 +0100

    started building the account activation page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ResetPassword from './components/ResetpasswordPage'
+import ActivateAccount from './components/ActivateAccount';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/passwordreset/:email" exact component={ResetPassword} />
+        <Route path="/activateaccount/:id" exact component={ActivateAccount} />
         <Route path='/' render={() => (<div>Route Not Found</div>)} />
      </Switch>
     </BrowserRouter>
---
+++ src/components/ActivateAccount.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ActivateAccount extends Component {
+  render() {
+    return (
+      <>
+        <div>Activate your account</div>
+      </>
+    )
+  }
+}
